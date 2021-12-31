d5b54f57ed8ad4f3af745f40546d5284b100af10
absurdTree <704459549@qq.com>
2021-12-31 10:52:29 +0800

    'test2'

--- src/views/home/arbitration/arbitrationList.vue
+++ src/views/home/arbitration/arbitrationList.vue
@@ -555,7 +555,7 @@
 			  this.zc_user_yq['show'] = true;
 			  this.zc_user_yq['my_user'] = address;
 			  var uname = await ArbOne.methods.message(address + "", "0").call();
-			  this.zc_user_yq['my_name'] = uname ? Base64.decode(uname) : '未填写1';
+			  this.zc_user_yq['my_name'] = uname ? Base64.decode(uname) : '未填写121';
 			  this.zc_user_yq['form_user'] = rows['user'];
 			  this.zc_user_yq['index'] = index;
 			  this.dialogFormVisible = true
@@ -568,6 +568,7 @@
 				    return;
 				  }
 					  console.log(111)
+					  console.log(222)
 				  this.dialogFormVisible = false
 				  var dq = this;
 				  //查询授权状态与GAZ余额
